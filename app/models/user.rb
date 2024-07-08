76f68828074280b641aabc028a63eef18ef9a159
Anuj <mohatkaranuj@gmail.com>
2024-07-09 02:48:03 +0530

    Overrided devise mailer method to use ActiveJob and deliver the mails later

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :tasks, dependent: :destroy
   validates :email, 'valid_email_2/email': {mx: true, disposable: true, disallow_subaddressing: true, dns_timeout: 30}
+
+  # Override the existing method so emails are delivered asynchronously and do not create a IO bound request.
+  def send_devise_notification(notification, *args)
+    message = devise_mailer.send(notification, self, *args)
+    
+    message.deliver_later
+  end
 end
